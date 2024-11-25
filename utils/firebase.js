45f6f4f5f968f6e7723ccb0e2280f87f82e2e3e8
usama khan <usamakhanu7860@gmail.com>
2024-11-25 18:33:58 +0530

    commit final

--- utils/firebase.js
+++ utils/firebase.js
@@ -8,13 +8,13 @@ import {getAuth} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC2ErVM7CL00A9QMjZNTwvJst2ZxVWazCw",
-  authDomain: "netflixgpt-313f4.firebaseapp.com",
-  projectId: "netflixgpt-313f4",
-  storageBucket: "netflixgpt-313f4.firebasestorage.app",
-  messagingSenderId: "80876173866",
-  appId: "1:80876173866:web:d7bcedc4871efbfa01a153",
-  measurementId: "G-ED0MWNT5TN"
+  apiKey: "AIzaSyByrgC4NOQKcx0CS_9IHnnTqQN_OyaY-hA",
+  authDomain: "netflixgpt-a1555.firebaseapp.com",
+  projectId: "netflixgpt-a1555",
+  storageBucket: "netflixgpt-a1555.firebasestorage.app",
+  messagingSenderId: "426976701715",
+  appId: "1:426976701715:web:8b47fef7cbbc4295933ee6",
+  measurementId: "G-35HRZMHX5G"
 };
 
 // Initialize Firebase
